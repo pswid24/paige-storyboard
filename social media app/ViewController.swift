0e570a3f9add15aad9d775b9f9704061a5e82c9e
paige swid <pswid24@cgps.org>
2022-11-30 10:41:03 -0500

    Initial Commit

---
+++ social media app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  social media app
+//
+//  Created by Paige on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
